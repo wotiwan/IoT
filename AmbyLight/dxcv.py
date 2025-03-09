201a52382df2304fcf76a2e4e9ebf9d747b949b9
wotiwan <146071322+wotiwan@users.noreply.github.com>
2025-03-09 19:51:33 +0800

    code refactored

--- AmbyLight/dxcv.py
+++ AmbyLight/dxcv.py
@@ -1,31 +1,19 @@
-import faulthandler
-faulthandler.enable()
-
 import dxcam
 import cv2
 import time
 import struct
 import serial
 
-print(dxcam.device_info())
-
-# Самая актуальная версия этот файл + прошивка ESP8266
-ser = serial.Serial('COM7', 1000000) # Бауд рейты прошивки и питон приложения не совпадают, но всё равно ->
-# -> оно работает
+ser = serial.Serial('COM7', 1000000)
 time.sleep(2)
 
-list_test = [255 for i in range(684)]
-
+# Аппаратное ускорения для увеличения быстродействия
 cv2.setUseOptimized(True)
-cv2.setNumThreads(8)  # Примерно +10%
+cv2.setNumThreads(8)
 cv2.ocl.setUseOpenCL(True)
 
-# SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600  # Пока вручную, позже добавить автоопределение
-
 # Размеры регионов
-TOP_HEIGHT = 100  # Верхняя область
-BOTTOM_HEIGHT = 100  # Нижняя область
-SIDE_WIDTH = 100  # Левая и правая области
+SIDES_DEPTH = 100
 
 
 def fps_counter():
@@ -71,39 +59,32 @@ def list_filling_side2(resized_array, col_arr):
             col_arr.append(j)
 
 
-
-camera = dxcam.create(output_color="RGB", output_idx=0)  # BGR вместо BGRA +63%
-camera.start(target_fps=140)
-
-fps_gen = fps_counter()
-
-
 def get_confirm(message: str):
     while True:
         if ser.in_waiting > 0:
             data = ser.readline().decode("utf-8", errors="replace").strip()
-            print(f"{data}")
+            # print(f"{data}")
             if data == message:
                 return True
-            if "�" in data:
-                print(data)
+            # No need now
+            # if "�" in data:
+            #     print(data)
 
 
-# Используется отправка пакетами вместо всего массива сразу
-# Во избежание переполнения буфера arduino и потери данных
 def send_array(col_arr):
     data_bytes = struct.pack(f'{1}B', 1) # Отправляем текущий режим
     ser.write(data_bytes)
-    # ser.flush()
-
     get_confirm("start")
 
-    cnt = 0
-    # Осталось только 24 значения, отправляем
     data_bytes = struct.pack(f'{684}B', *col_arr)
     ser.write(data_bytes)
-    # ser.flush()
     get_confirm("OK_ambi")
+
+
+camera = dxcam.create(output_color="RGB", output_idx=0)  # BGR вместо BGRA +63%
+camera.start(target_fps=40)
+
+fps_gen = fps_counter()
 
 while True:
 
@@ -111,16 +92,16 @@ while True:
 
     frame = camera.get_latest_frame()
     if frame is None:
-        print("NOOOOOOOONE")
+        print("New frame is missing!")
         continue
 
-    top_part = frame[:TOP_HEIGHT, :, :3]  # Верхняя полоса (ширина экрана, 100px)
-    bottom_part = frame[-BOTTOM_HEIGHT:, :, :3]  # Нижняя полоса (ширина экрана, 100px)
-    left_part = frame[:, :SIDE_WIDTH, :3]  # Левая полоса (100px, высота экрана)
-    right_part = frame[:, -SIDE_WIDTH:, :3]  # Правая полоса (100px, высота экрана)
+    # Выделение нужных секторов
+    top_part = frame[:SIDES_DEPTH, :, :3]  # Верхняя полоса (ширина экрана, 100px)
+    bottom_part = frame[-SIDES_DEPTH:, :, :3]  # Нижняя полоса (ширина экрана, 100px)
+    left_part = frame[:, :SIDES_DEPTH, :3]  # Левая полоса (100px, высота экрана)
+    right_part = frame[:, -SIDES_DEPTH:, :3]  # Правая полоса (100px, высота экрана)
 
-
-    # Этот кусок ест 2-4 фпс
+    # Уменьшение секторов с аппроксимацией по цвету
     top_resized = cv2.reduce(top_part, 0, cv2.REDUCE_AVG)
     top_resized = cv2.resize(top_resized, (73, 1), cv2.INTER_AREA)
 
@@ -133,18 +114,17 @@ while True:
     right_resized = cv2.reduce(right_part, 1, cv2.REDUCE_AVG)
     right_resized = cv2.resize(right_resized, (1, 41), cv2.INTER_AREA)
 
-
-    # Этот кусок кода не ест производительность
+    # Заполняем массив цветов в правильном порядке
     list_filling(top_resized, color_array)
     list_filling_side(right_resized, color_array)
     list_filling2(bottom_resized, color_array)
     list_filling_side2(left_resized, color_array)
 
 
-    send_array(color_array) # Отправка пакетами
+    send_array(color_array) # Отправка пакета
 
     fps = next(fps_gen)
-    # print(f"FPS: {fps:.2f}")
+    print(f"FPS: {fps:.2f}")
 
 camera.stop()
 cv2.destroyAllWindows()
