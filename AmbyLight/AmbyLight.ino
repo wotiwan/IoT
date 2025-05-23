78871e3d9a76e1effd7a19848998e54662957a50
wotiwan <146071322+wotiwan@users.noreply.github.com>
2025-05-23 12:53:51 +0800

    Update AmbyLight.ino

--- AmbyLight/AmbyLight.ino
+++ AmbyLight/AmbyLight.ino
@@ -1,130 +1,388 @@
-import dxcam
-import cv2
-import time
-import struct
-import serial
+#include <WiFiManager.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <FastLED.h>
 
-ser = serial.Serial('COM3', 2000000)
-time.sleep(2)
+// MQTT Setup
+const char* mqtt_server = "176.215.235.238"; // 176.215.235.238 or 192.168.43.133
+const int mqtt_port = 1884;
+const char* mqtt_topic = "led/data";
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-# Аппаратное ускорения для увеличения быстродействия
-cv2.setUseOptimized(True)
-cv2.setNumThreads(8)
-cv2.ocl.setUseOpenCL(True)
+// FastLED Setup
+#define LED_PIN 22
+#define LED_COUNT 228
+CRGB leds[LED_COUNT]; // Объявление массива светодиодов
 
-# Размеры регионов
-SIDES_DEPTH = 100
+// Local settings setup
+int mode = 3; // Изначальный режим не должен ждать никаких значений.
+// Color calibration
+// TODO: input by user
+float red_ratio = 1.05;
+float green_ratio = 0.98;
+float blue_ratio = 0.96;
 
+float smooth_ratio = 0.3; // Переменная для сглаживания картинки ambient режима
+int saturation = 255; // Насыщенность HSV цвета, задаётся пользователем ?? Если колорпикер будет хсвшным, что вряд ли
+int brightness = 85; // Яркость, задаётся пользователем, по умолчанию (!максимальная)
+int effects_speed = 10; // Скорость световых эффектов, шкала от 1 до 10, задаётся пользователем
 
-def fps_counter():
-    frame_count = 0
-    start_time = time.time()
-    while True:
-        yield frame_count / (time.time() - start_time + 1e-6)
-        frame_count += 1
-        if time.time() - start_time >= 1.0:
-            frame_count = 0
-            start_time = time.time()
+int global_rgb[3] = {255, 0, 0};
+int global_hsv = 0;
+int hsv_states[LED_COUNT] = {0};
+bool rainbow_set = false;
+bool gradient_set = false;
+bool star_set = false;
 
-def list_filling(resized_array, col_arr):
-    for i in resized_array[0]:
-        for j in i:
-            col_arr.append(j)
+int star_position = 73; // стартовая позиция
 
+// MQTT vars
+byte savedPayload[1024];
+unsigned int savedPayloadLength = 0;
 
-def list_filling2(resized_array, col_arr):
-    temp_arr = []
-    for i in resized_array[0]:
-        temp_arr.append(i)
-    len_arr = len(temp_arr)
-    for i in range(len_arr-1,-1,-1):
-        for j in temp_arr[i]:
-            col_arr.append(j)
+unsigned long start_time = 0;
+unsigned long cur_time = 0;
 
+void off_the_lights();
+void ambilight();
+void static_lights();
+void set_rainbow();
+void rainbow();
+void set_gradient();
+void gradient();
+void set_star();
+void star_shooting();
 
-def list_filling_side(resized_array, col_arr):
-    for i in resized_array:
-        for j in i[0]:
-            col_arr.append(j)
+void change_brightness();
+void change_rgb_color();
 
+void custom_delay();
 
-def list_filling_side2(resized_array, col_arr):
-    temp_arr = []
-    for i in resized_array:
-        for j in i:
-            temp_arr.append(j)
-    len_arr = len(temp_arr)
-    for i in range(len_arr - 1, -1, -1):
-        for j in temp_arr[i]:
-            col_arr.append(j)
+int find_max(int a, int b, int c);
 
+void callback_on_data(char* topic, byte* payload, unsigned int length);
+void mqtt_reconnect();
+void timer_start();
+void timer_end();
 
-def get_confirm(message: str):
-    while True:
-        if ser.in_waiting > 0:
-            data = ser.readline().decode("utf-8", errors="replace").strip()
-            print(f"{data}")
-            if data == message:
-                return True
-            # No need now
-            # if "�" in data:
-            #     print(data)
+void setup() {
+  Serial.setRxBufferSize(1024);
+  Serial.begin(2000000);
+  setCpuFrequencyMhz(240);
+  pinMode(LED_PIN, OUTPUT);
 
+  // MQTT Setup
+  WiFiManager wifiManager; 
+  wifiManager.autoConnect();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback_on_data);
+  mqtt_reconnect();
 
-def send_array(col_arr):
-    data_bytes = struct.pack(f'{1}B', 1) # Отправляем текущий режим
-    ser.write(data_bytes)
-    # get_confirm("start") # не имеет смысла
+  //FastLED Setup
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_COUNT);
+}
 
-    data_bytes = struct.pack(f'{684}B', *col_arr)
-    ser.write(data_bytes)
-    get_confirm("OK_ambi")
+void loop() {
 
+  if (mode == 0) { 
+    off_the_lights();
+  }
+  if (mode == 2) { // Режим статичного цвета
+    static_lights();
+  }
+  if (mode == 3) {
+    rainbow();
+  }
+  if (mode == 4) {
+    gradient();
+  }
+  if (mode == 5) {
+    star_shooting();
+  }
 
-camera = dxcam.create(output_color="RGB", output_idx=0)  # BGR вместо BGRA +63%
-camera.start(target_fps=30)
+  if (!client.connected()) {
+    mqtt_reconnect();
+  }
+  client.loop();
+}
 
-fps_gen = fps_counter()
+void off_the_lights() {
+  for (int i = 0; i < LED_COUNT; i++) {
+    leds[i] = 0;
+  }
+  FastLED.show();
+  Serial.println("OK_off");
+}
 
-while True:
+void static_lights() {
+  for (int i = 0; i < LED_COUNT; i++) {
+    leds[i].r = global_rgb[0]; // Копируем полученный цвет во все диоды
+    leds[i].g = global_rgb[1];
+    leds[i].b = global_rgb[2];
+  }
+  FastLED.show();
+}
 
-    color_array = []
+void ambilight() {
+  int led_index = 0;
+  for (int i = 1; i < savedPayloadLength; i+=3, led_index++) { 
+    int new_r = savedPayload[i];
+    int new_g = savedPayload[i+1];
+    int new_b = savedPayload[i+2];
 
-    frame = camera.get_latest_frame()
-    if frame is None:
-        print("New frame is missing!")
-        continue
+    if (new_r <= 20 && new_g <= 20 && new_b <= 20) { // Небольшой трешхолд для тусклых серых цветов
+      new_r = 0;
+      new_g = 0;
+      new_b = 0;
+    }
+    else {
 
-    # Выделение нужных секторов
-    top_part = frame[:SIDES_DEPTH, :, :3]  # Верхняя полоса (ширина экрана, 100px)
-    bottom_part = frame[-SIDES_DEPTH:, :, :3]  # Нижняя полоса (ширина экрана, 100px)
-    left_part = frame[:, :SIDES_DEPTH, :3]  # Левая полоса (100px, высота экрана)
-    right_part = frame[:, -SIDES_DEPTH:, :3]  # Правая полоса (100px, высота экрана)
+      float color_ratio = 0;
 
-    # Уменьшение секторов с аппроксимацией по цвету
-    top_resized = cv2.reduce(top_part, 0, cv2.REDUCE_AVG)
-    top_resized = cv2.resize(top_resized, (73, 1), cv2.INTER_AREA)
+      int brightest_color = find_max(new_r, new_g, new_b);
+      // Если тусклый цвет, то вычитаем белый компонент, затем увеличиваем яркость с большим коэфф.
+      if (brightest_color < 80 and brightest_color > 30) { // Другими словами увеличиваем насыщенность цвета
+        new_r = max(new_r - 7, 0); // 14 
+        new_g = max(new_g - 7, 0); // 24
+        new_b = max(new_b - 7, 0); // 4
+        color_ratio = (255 - brightest_color - 10) / 255 * 1.35; // 1.65 // Коэф. также подстраивается
+      }
+      else if (brightest_color > 30) {
+        new_r = max(new_r - 15, 0); 
+        new_g = max(new_g - 15, 0);
+        new_b = max(new_b - 15, 0);
+        color_ratio = (255 - brightest_color) / 255; // Увеличение яркости тусклых пикселей
+      }
 
-    bottom_resized = cv2.reduce(bottom_part, 0, cv2.REDUCE_AVG)
-    bottom_resized = cv2.resize(bottom_resized, (73, 1), cv2.INTER_AREA)
+      new_r = new_r * (color_ratio + 1) * red_ratio; // 37
+      new_g = new_g * (color_ratio + 1) * green_ratio; // 64
+      new_b = new_b * (color_ratio + 1) * blue_ratio; // 11
+    }
 
-    left_resized = cv2.reduce(left_part, 1, cv2.REDUCE_AVG)
-    left_resized = cv2.resize(left_resized, (1, 41), cv2.INTER_AREA)
+    // Устраняем мерцание сглаживанием
+    leds[led_index].r = leds[led_index].r * (1 - smooth_ratio) + new_r * smooth_ratio;  // Читаем R
+    leds[led_index].g = leds[led_index].g * (1 - smooth_ratio) + new_g * smooth_ratio;  // Читаем G
+    leds[led_index].b = leds[led_index].b * (1 - smooth_ratio) + new_b * smooth_ratio;  // Читаем B
 
-    right_resized = cv2.reduce(right_part, 1, cv2.REDUCE_AVG)
-    right_resized = cv2.resize(right_resized, (1, 41), cv2.INTER_AREA)
+  }
 
-    # Заполняем массив цветов в правильном порядке
-    list_filling(top_resized, color_array)
-    list_filling_side(right_resized, color_array)
-    list_filling2(bottom_resized, color_array)
-    list_filling_side2(left_resized, color_array)
+  // Serial.println("Time for garbage per cycle (ms):");
+  while(Serial.available()) { 
+    Serial.read();
+  }
 
+  FastLED.show();
+  Serial.println("OK_ambi");
+}
 
-    send_array(color_array) # Отправка пакета
+void set_rainbow() {
+  for (int i = 0; i < LED_COUNT; i++) { // установка стартового состояния
+    if (global_hsv >= 255) { // Если дошли до предела - перешли на следующий круг
+      global_hsv = global_hsv % 255;
+    }
+    leds[i].setHSV(global_hsv, 255, brightness);
+    hsv_states[i] = global_hsv;
 
-    fps = next(fps_gen)
-    print(f"FPS: {fps:.2f}")
+    if (i % 4 == 0) { // Для равномерного распределния цветов на 228 светодиодов
+      global_hsv += 3;
+    } else {
+      global_hsv += 2;
+    }
+  }
+  FastLED.show();
+  rainbow_set = true;
 
-camera.stop()
-cv2.destroyAllWindows()
+}
+
+void rainbow() {
+  if (!rainbow_set) {
+    set_rainbow();
+  }
+
+  for (int i = 0; i < LED_COUNT; i++) {
+    int new_color = (hsv_states[i] + 1) % 256;
+    hsv_states[i] = new_color;
+    leds[i].setHSV(new_color, 255, brightness);
+  }
+  FastLED.show();
+  custom_delay();
+}
+
+void set_gradient() {
+  global_hsv = 200;
+  for (int i = 0; i < LED_COUNT; i++) {
+    leds[i].setHSV(global_hsv, 255, brightness);
+  }
+  gradient_set = true;
+}
+
+void gradient() {
+
+  if (!gradient_set) {
+    set_gradient();
+    FastLED.show();
+    return;
+  }
+
+  for (int i = 0; i < LED_COUNT; i++) {
+    leds[i].setHSV(global_hsv, 255, brightness);
+  }
+
+  global_hsv = (global_hsv + 1) % 256; // обнуляем если вылезли за предел
+
+  FastLED.show();
+  custom_delay();
+}
+
+void set_star() {
+
+  off_the_lights();
+
+  leds[star_position].r = global_rgb[0];
+  leds[star_position].g = global_rgb[1];
+  leds[star_position].b = global_rgb[2];
+  FastLED.show();
+  custom_delay();
+  star_set = true;
+}
+
+void star_shooting() {
+
+  if (!star_set) {
+    set_star();
+  }
+
+  int star_brightness = brightness;
+  for (int i = star_position; i > 72; i-- ) {
+    
+    int brightness_ratio = (star_position - i) * 20; // степень затухания
+    if (i > 113) { // Если ушли за край экрана (стороны)
+      leds[i] = 0;
+    } 
+    else {
+      leds[i].r = max(global_rgb[0] - brightness_ratio, 0);
+      leds[i].g = max(global_rgb[1] - brightness_ratio, 0);
+      leds[i].b = max(global_rgb[2] - brightness_ratio, 0);
+    }
+  }
+  
+  star_position = min(star_position+1, 127);
+  if (star_position == 127) {
+    star_set = false;
+    star_position = 73;
+  }
+  FastLED.show();
+  custom_delay();
+}
+
+void custom_delay() { 
+  long int start_time = millis();
+  long int cur_time = millis();
+  int delay_ratio = 11 - effects_speed;
+  int delay = 15 * delay_ratio;
+  while (true) {
+    if (cur_time - delay >= start_time) {
+      return;
+    }
+    cur_time = millis();
+  }
+}
+
+void change_brightness() {
+  while (Serial.available() < 1) {
+    // Ожидаем пакет
+  }
+  brightness = Serial.read();
+  while(Serial.available()) {
+    Serial.read();
+  }
+  Serial.println("OK_brightness");
+}
+
+void change_rgb_color() {
+  Serial.println("start_rgb");
+  while (Serial.available() < 3) {
+    delay(1); // Ожидаем пакет // Сменить на кастом
+  }
+  global_rgb[0] = Serial.read();
+  global_rgb[1] = Serial.read();
+  global_rgb[2] = Serial.read();
+  while(Serial.available()) {
+    Serial.read();
+  }
+  Serial.println("OK_rgb");
+}
+
+int find_max(int a, int b, int c) {
+  if (a > b && a > c) {
+    return a;
+  }
+  else if (b > a && b > c) {
+    return b;
+  }
+  else {
+    return c;
+  }
+}
+
+void callback_on_data(char* topic, byte* payload, unsigned int length) {
+  // Serial.println("Time taken for callback wait (ms): ");
+  timer_end();
+  // Обработка
+  int new_mode = payload[0];
+
+  memcpy(savedPayload, payload, length);  // копируем байты
+  savedPayloadLength = length;
+  
+  // Смена цвета, яркости, насыщенности не влечёт за собой смену режима
+  if (new_mode == 99) { // Меняем яркость
+    change_brightness();
+  } 
+  else if (new_mode == 98) { // Меняем цвет
+    change_rgb_color();
+  }
+  else {
+    mode = new_mode;
+  }
+  // Serial.println(mode);
+  if (mode != 3) {
+    rainbow_set = false;
+    global_hsv = 0;
+  }
+  if (mode != 4) {
+    gradient_set = false;
+    global_hsv = 0; // потом поломается это, надо по хорошему разные переменные хсв для градиента и радуги
+  }
+  star_set = false;
+  
+  if (mode == 1) { // Адаптивная эмбиент подсветка
+    ambilight();
+  }
+  timer_start();
+}
+
+void mqtt_reconnect() {
+  while (!client.connected()) {
+    Serial.print("Подключение к MQTT...");
+    String clientId = "ESP32Client-" + String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("Подключено!");
+      client.subscribe(mqtt_topic);
+    } else {
+      Serial.print("Ошибка, rc=");
+      Serial.print(client.state());
+      Serial.println(" повтор через 5 сек");
+      delay(5000);
+    }
+  }
+}
+
+void timer_start() {
+  start_time = millis();
+}
+
+void timer_end() {
+  cur_time = millis();
+  // Serial.println(cur_time - start_time);
+}
